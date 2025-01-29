19b9eb59227a2fd6e73b5f2e71d26e31d21845e1
alejoboga20 <alejoboga19@gmail.com>
2025-01-29 10:28:50 -0300

    implement sorted array of integers

--- grokking-data-structures/03-sorted-arrays/main.py
+++ grokking-data-structures/03-sorted-arrays/main.py
@@ -1,10 +1,21 @@
-from typing import Generic, List, TypeVar, Optional
+class SortedArray():
+    def __init__(self, size: int) -> None:
+        self.filled_index: int = 0
+        self.size: int = size
+        self.data: list[int] = [0] * size
 
-T = TypeVar('T')
+    def __str__(self):
+        str_array = str(self.data)
+
+        return str_array
+
+    def __validate_insert__(self):
+        """Check if the array is already full"""
+        if (self.filled_index == self.size):
+            raise IndexError("Array already full")
+
+    def insert_element(self, element: int):
+        print("insert element")
 
 
-class SortedArray(Generic[T]):
-    def __init__(self, size: int, default_value: Optional[T] = None) -> None:
-        self.size = size
-        self.data: List[Optional[T]] = [default_value] * size
-        self.element_type: Optional[type] = type(default_value) if default_value is not None else None
+sorted_array = SortedArray(10)
