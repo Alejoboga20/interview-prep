8ed9e53d742c974500ab68dfa772a1a96671f705
alejoboga20 <alejoboga19@gmail.com>
2025-02-02 12:14:00 -0300

    implement node insertion

--- grokking-data-structures/06-linked-lists/singly-linked-list.py
+++ grokking-data-structures/06-linked-lists/singly-linked-list.py
@@ -1,22 +1,49 @@
 class ListNode:
-    def __init__(self, data, next_node=None):
-        self._data = data
-        self._next = next_node
-
-    def data(self):
-        return self._data
-
-    def next(self):
-        return self._next
-
-    def hast_next(self):
-        is_tail = True if self._next is not None else False
-        return is_tail
-
-    def append(self, next_node):
-        self._next = next_node
+    def __init__(self, value):
+        self.value = value
+        self.next: ListNode | None = None
 
 
 class SinglyLinkedList:
     def __init__(self):
-        self._head = None
+        self.head: ListNode | None = None
+        self.size = 0
+
+    def __str__(self):
+        """Return a string representation of the list"""
+        current_node = self.head
+        list_values = []
+
+        while current_node is not None:
+            list_values.append(str(current_node.value))
+            current_node = current_node.next
+
+        formatted_list_values = " -> ".join(list_values)
+
+        return formatted_list_values
+
+    def is_empty(self):
+        """Check if the list is empty"""
+        is_empty_list = self.size == 0
+        return is_empty_list
+
+    def insert_element(self, value):
+        """Intert a new node into the list"""
+        new_node = ListNode(value)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+
+        self.size += 1
+
+
+linked_list = SinglyLinkedList()
+linked_list.insert_element(1)
+linked_list.insert_element(2)
+linked_list.insert_element(3)
+print(f"list: {linked_list.__str__()}, size: {linked_list.size}")
