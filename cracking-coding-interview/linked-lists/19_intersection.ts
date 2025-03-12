650c9428462379669dc9a874531e2707496cfcdf
alejoboga20 <alejoboga19@gmail.com>
2025-03-12 16:37:33 -0300

    intersection node

---
+++ cracking-coding-interview/linked-lists/19_intersection.ts
@@ -0,0 +1,25 @@
+// 8.  *Intersection*;
+
+// Given two (singly) linked lists, determine if the two lists intersect.
+// Return the first intersecting node. Note that the intersection is defined
+// based on reference, not value.
+
+import { SinglyLinkedList, ListNode } from './10_LinkedList';
+
+export default function intersection<T>(
+	list1: SinglyLinkedList<T>,
+	list2: SinglyLinkedList<T>
+): ListNode<T> | undefined {
+	let intersectionNode: ListNode<T> | undefined = undefined;
+	const set = new Set<ListNode<T>>();
+
+	list1.visit((listNode) => set.add(listNode));
+
+	list2.visit((listNode) => {
+		if (set.has(listNode) && !intersectionNode) {
+			intersectionNode = listNode;
+		}
+	});
+
+	return intersectionNode;
+}
