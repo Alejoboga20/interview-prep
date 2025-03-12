2e706e5a3c6af99853282fb33d1d953e7b9b4e42
alejoboga20 <alejoboga19@gmail.com>
2025-03-12 16:28:25 -0300

    palindrome list

---
+++ cracking-coding-interview/linked-lists/17_palindrome.ts
@@ -0,0 +1,19 @@
+// 7. *Palindrome*:
+
+// Implement a function to check if a linked list is a palindrome.
+
+import { SinglyLinkedList, ListNode } from './10_LinkedList';
+
+export default function isPalindrome<T>(list: SinglyLinkedList<T>): boolean {
+	let word = '';
+
+	list.visit((listNode) => (word += listNode.value));
+
+	for (let index = 0; index < word.length / 2; index++) {
+		if (word[index] !== word[word.length - 1 - index]) {
+			return false;
+		}
+	}
+
+	return true;
+}
