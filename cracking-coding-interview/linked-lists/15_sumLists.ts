32fb6416ccb4e19bf757e92826b9b8fcbf339138
alejoboga20 <alejoboga19@gmail.com>
2025-03-12 15:53:48 -0300

    sum list

---
+++ cracking-coding-interview/linked-lists/15_sumLists.ts
@@ -0,0 +1,59 @@
+// 5. *Sum Lists*: You have two numbers represented by a linked list,
+// where each node contains a single digit. The digits are stored in reverse order,
+// such that the Vs digit is at the head of the list.
+// Write a function that adds the two numbers and returns the sum as a linked list.
+
+// ```
+// EXAMPLE
+// Input: (7-> 1 -> 6) + (5 -> 9 -> 2).That is,617 + 295.
+// Output: 2 -> 1 -> 9. That is, 912.
+// ```
+
+import { SinglyLinkedList, ListNode } from './10_LinkedList';
+
+export default function sumLists(
+	firstList: SinglyLinkedList<number>,
+	secondList: SinglyLinkedList<number>
+): SinglyLinkedList<number> {
+	const sumList = new SinglyLinkedList<number>();
+
+	let firstPointer: ListNode<number> | undefined = firstList.head;
+	let secondPointer: ListNode<number> | undefined = secondList.head;
+
+	let carrier = 0;
+
+	while (firstPointer || secondPointer) {
+		const firstValue = firstPointer ? firstPointer.value : 0;
+		const secondValue = secondPointer ? secondPointer.value : 0;
+
+		let sumValue = firstValue + secondValue + carrier;
+		carrier = 0;
+
+		if (sumValue >= 10) {
+			carrier = 1;
+			sumValue = sumValue - 10;
+		}
+		sumList.push(sumValue);
+
+		firstPointer = firstPointer?.next;
+		secondPointer = secondPointer?.next;
+	}
+
+	if (carrier !== 0) {
+		sumList.push(carrier);
+	}
+
+	return sumList;
+}
+const firstList = new SinglyLinkedList<number>();
+firstList.push(7);
+firstList.print();
+
+const secondList = new SinglyLinkedList<number>();
+secondList.push(5);
+secondList.push(9);
+secondList.push(8);
+secondList.print();
+
+const sumList = sumLists(firstList, secondList);
+sumList.print();
