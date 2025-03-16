be88b52509b2f0a2baed76223378c40ac72d066a
alejoboga20 <alejoboga19@gmail.com>
2025-03-16 15:51:22 -0300

    find max

--- grokking-algorithms/03-recursion/findMax.ts
+++ grokking-algorithms/03-recursion/findMax.ts
@@ -5,7 +5,13 @@ Example:
 arr = [1, 2 ,6, 2, 9, 12] -> max = 12
  */
 export const findMax = (numberArr: number[]): number => {
-	let max = 0;
+	if (numberArr.length === 1) {
+		return numberArr[0];
+	}
 
-	return max;
+	const maxOfRest = findMax(numberArr.slice(1));
+
+	return numberArr[0] > maxOfRest ? numberArr[0] : maxOfRest;
 };
+
+console.log(findMax([1, 2, 6, 2, 9, 12])); // Output: 12
