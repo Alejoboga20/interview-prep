7082bbafaeadd4773b1518e80b031da9ec8bad47
alejoboga20 <alejoboga19@gmail.com>
2025-03-17 09:32:12 -0300

    use pivot in the middle

--- grokking-algorithms/04-quicksort/quicksort.ts
+++ grokking-algorithms/04-quicksort/quicksort.ts
@@ -12,5 +12,27 @@ export const quicksort = (arr: number[] = []): number[] => {
 	return quicksort(less).concat([pivot]).concat(quicksort(greater));
 };
 
+export const quicksortWithCustomPivot = (arr: number[]): number[] => {
+	const arrLength = arr.length;
+
+	if (arrLength < 2) {
+		return arr;
+	}
+
+	const pivotIndex = Math.floor(arrLength / 2);
+	const pivot = arr[pivotIndex];
+
+	const less = arr.filter(
+		(element, index) => index !== pivotIndex && element <= pivot
+	);
+	const greater = arr.filter(
+		(element, index) => index !== pivotIndex && element > pivot
+	);
+
+	return quicksort(less).concat([pivot]).concat(quicksort(greater));
+};
+
 console.log(quicksort([10, 5, 2, 3]));
+console.log(quicksortWithCustomPivot([10, 5, 2, 3]));
 console.log(quicksort([10, 5, 2, 32, 13, 1, 15, 345, 23]));
+console.log(quicksortWithCustomPivot([10, 5, 2, 32, 13, 1, 15, 345, 23]));
