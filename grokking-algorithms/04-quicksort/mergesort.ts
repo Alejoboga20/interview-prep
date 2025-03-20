4e447710a121c65873b4f8161c4e07382808fd5f
alejoboga20 <alejoboga19@gmail.com>
2025-03-20 14:21:10 -0300

    merge sort

---
+++ grokking-algorithms/04-quicksort/mergesort.ts
@@ -0,0 +1,36 @@
+/* 
+Time Complexity:
+Best Case: O(n log n)
+Worst Case: O(n log n)
+Average Case: O(n log n)
+*/
+
+export const mergesort = (arr: number[]): number[] => {
+	if (arr.length <= 1) return arr; // base case
+
+	const middle = Math.floor(arr.length / 2);
+	const left = mergesort(arr.slice(0, middle));
+	const right = mergesort(arr.slice(middle));
+
+	return merge(left, right);
+};
+
+const merge = (left: number[], right: number[]): number[] => {
+	let sortedArray: number[] = [];
+	let leftIndex = 0;
+	let rightIndex = 0;
+
+	while (leftIndex < left.length && rightIndex < right.length) {
+		if (left[leftIndex] < right[rightIndex]) {
+			sortedArray.push(left[leftIndex]);
+			leftIndex++;
+		} else {
+			sortedArray.push(right[rightIndex]);
+			rightIndex++;
+		}
+	}
+
+	return sortedArray
+		.concat(left.slice(leftIndex))
+		.concat(right.slice(rightIndex));
+};
