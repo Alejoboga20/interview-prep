8b3ff61f8f26c83ddd790d363e4ee57d649b8b72
alejoboga20 <alejoboga19@gmail.com>
2025-08-13 16:28:12 -0300

    pop list

--- hands-on-ds-algo/04-arrays-linked-lists/singly-linked-list.ts
+++ hands-on-ds-algo/04-arrays-linked-lists/singly-linked-list.ts
@@ -18,12 +18,14 @@ export class SinglyLinkedList<T> {
 		this.tail = null;
 	}
 
+	// O(1)
 	getLength(): number {
 		const length = this.length;
 
 		return length;
 	}
 
+	// O(n)
 	traverse(): void {
 		let current = this.head;
 		let counter = 0;
@@ -36,6 +38,7 @@ export class SinglyLinkedList<T> {
 		}
 	}
 
+	// O(1)
 	push(value: T): void {
 		const newNode = new Node(value);
 
@@ -49,5 +52,35 @@ export class SinglyLinkedList<T> {
 		this.tail.next = newNode;
 		this.tail = newNode;
 		this.length++;
+	}
+
+	// O(n)
+	pop() {
+		if (!this.head) return null;
+
+		if (this.head === this.tail) {
+			const value = this.head.value;
+			this.head = null;
+			this.tail = null;
+
+			this.length--;
+			return value;
+		}
+
+		let prev = this.head;
+		let current = this.head.next!;
+
+		while (current.next) {
+			prev = current;
+			current = current.next;
+		}
+
+		prev.next = null;
+		this.tail = prev;
+		this.length--;
+
+		const value = current.value;
+
+		return value;
 	}
 }
